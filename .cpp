f6b5b811bdf067c59c86bf7353556327fb07e05f
KiaraRemuzgo <61111576@continental.edu.pe>
2026-06-05 12:44:16 -0500

    Update .cpp

---
+++ .cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string>
+#include <cstdlib> 
+
+using namespace std;
+
+class alumno
+{    private:
+	         string nombre;
+	         string carrera;
+	 public:
+	         alumno(); 
+			 ~alumno();
+			 void ingresar();
+			 void imprimir();       
+};
+
+alumno::alumno()
+{   nombre=" ";
+    carrera=" ";
+}
+
+alumno::~alumno()
+{
+}
+
+void alumno::ingresar()
+{
+	cout<<" ";
+	cin>>nombre;
+	cout<<"Ingrese carrera: "; 
+	cin>>carrera;
+    
+}
+
+void alumno::imprimir() 
+{
+	cout<<"Nombre Alumno: ";
+	cout<<nombre<<"\n";
+	cout<<"Carrera: ";
+	cout<<carrera<<"\n";
+}
+
+int main() 
+{	alumno al;
+
+    cout<<"Ingrese Alumno\n";
+    al.ingresar();cout<<"\n";
+    
+    cout<<"Mostrar Alumno\n";
+    al.imprimir();cout<<"\n\n";
+    system("PAUSE");
+    
+    cout<<"Eliminar Alumno\n";
+    al = alumno(); 
+    cout<<"\n";
+    system("PAUSE");
+    
+    cout<<"Mostrar Alumno\n";
+    al.imprimir();cout<<"\n";
+    
+system("PAUSE");
+return 0;
+}
